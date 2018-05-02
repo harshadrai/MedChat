340ca79f88d56b8a27730d0e5b295da9d0f76bcb
Harshad Rai <hrrai2@illinois.edu>
2018-05-01 19:24:26 -0500

    Accessing all symptoms

--- functions/index.js
+++ functions/index.js
@@ -9,22 +9,76 @@ var firestore = admin.firestore();
 //
 exports.webhook = functions.https.onRequest((request, response) => {
 
-	console.log("request.body.result.parameters: ", request.body.result.parameters);
+console.log("request.body.result.parameters: ", request.body.result.parameters);
 
-	let params = request.body.result.parameters;
+let action = request.body.result.action;
 
-	firestore.collection('Symptoms').add(params).then(() => {
+switch (action) {
+	case 'saveSymptoms':{
+
+		let params = request.body.result.parameters;
+
+		var docRef = firestore.collection('Symptoms')
+
+		docRef.add(params).then(() => {
+			// response.send({
+			// 	speech: 'Your symptoms have been added to the database!'
+			// });
+			return;
+		}).catch((error => {
+			console.log("error: ", e);
+			response.send({
+				speech:"Something went wrong while writing on database"
+			});
+			return;
+		}))
+	break;
+	}
+
+	case 'findCloseSymptoms':{
+
+		firestore.collection('Symptoms').get().then((querySanpshot) =>{
+			var listOfSymptomsList = [];
+			querySanpshot.forEach((doc) => {listOfSymptomsList.push(doc.data())});
+		
+			var symptoms = [];
+			// var speech = '';
+
+			listOfSymptomsList.forEach((symptomsList, index) => {
+				// symptomsList.Symptoms.forEach((symptom, ind) => {
+					symptoms.push(symptomsList.Symptoms)
+				// }
+			})
+			// speech += symptomsList.Symptoms
+			// })
+
+			response.send({
+				speech: 'All of the symptoms: ' + symptoms
+			});
+
+
+			// Write the remaining part of the code here
+
+
+			return;
+		}).catch((err) => {
+			console.log('Error getting symptoms from database', err);
+			response.send({
+				speech: "Something went wrong while reading the database"
+			});
+			return;
+		})
+	break;
+	}
+
+	default:{
 		response.send({
-			speech: 'Your symptoms have been added to the database!'
+			speech: 'No action matched.'
 		});
-		return;
-	}).catch((error => {
-		console.log("error: ", e);
-		response.send({
-			speech:"Something went wrong while writing on database"
-		});
-		return;
-	}))
+	}
+}
+
+
 	// response.send({
 	//  	speech:
 	//  		'New. You have the following symptoms ' + params.Symptoms
